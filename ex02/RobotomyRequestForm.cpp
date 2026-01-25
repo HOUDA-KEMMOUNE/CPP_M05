7bb376eabb99e758d95bfdc4f00c8d87da80996b
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2026-01-25 04:47:38 +0100

    exceptions

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,19 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm( const std::string &t )
+	: AForm(72, 45)
+{
+	target = t;
+}
+
+RobotomyRequestForm::RobotomyRequestForm( const RobotomyRequestForm &old )
+{
+	*this = old;
+}
+
+RobotomyRequestForm	&RobotomyRequestForm::operator=( const RobotomyRequestForm &old )
+{
+	if (this != &old)
+		target = old.target;
+	return (*this);
+}
